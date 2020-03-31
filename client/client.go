e53d32c8b40ec0704825f60563750eea1a18dcf1
Eugene Shevekhman <yevshev@users.noreply.github.com>
2020-03-30 22:40:59 -0500

    scale to 100 nodes

--- client/client.go
+++ client/client.go
@@ -51,8 +51,8 @@ func collectCPUTemperature(hostname string) {
 
 func main() {
 
-	// Poll 200 servers
-	var nodeList [200]string
+	// Poll 100 servers
+	var nodeList [100]string
 
 	// Fill array with server hostnames
 	for i := range nodeList {
